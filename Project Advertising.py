06076dd823793f97a999fc8ff7631e612007c627
Livetomuch <117250891+Livetomuch@users.noreply.github.com>
2023-03-07 14:18:25 +0800

    Update Project Advertising.py

--- Project Advertising.py
+++ Project Advertising.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-Advert = pd.read_csv("Advertising.csv")
+Advert = pd.read_csv("https://github.com/Livetomuch/Liveto/blob/a6c8556261e3252bccf1c98c6c18b6dcd6956e98/Advertising.csv")
 st.write("""
 # Advertising to Sales Prediction App
 """)
