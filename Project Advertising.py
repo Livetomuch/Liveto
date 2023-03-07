414c3ac33ceb3c6bd270b322106d4fcc8da64acf
Livetomuch <117250891+Livetomuch@users.noreply.github.com>
2023-03-07 14:43:12 +0800

    Update Project Advertising.py

--- Project Advertising.py
+++ Project Advertising.py
@@ -2,10 +2,27 @@ import streamlit as st
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-url = 'https://raw.githubusercontent.com/livetomuch/liveto/main/Advertising.csv'
-Advert = pd.read_csv(url)
+
 st.write("""
 # Advertising to Sales Prediction App
 """)
 
 st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV = st.sidebar.slider('TV', 0, 200, 54)
+    Radio = st.sidebar.slider('Radio', 0, 100, 30)
+    Newspaper = st.sidebar.slider('Newspaper', 0, 100, 20)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper,}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+#url = 'https://raw.githubusercontent.com/livetomuch/liveto/main/Advertising.csv'
+#Advert = pd.read_csv(url)
