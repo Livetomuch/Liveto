dbc8d459f407b840608731993f33a0802c6ead87
Livetomuch <117250891+Livetomuch@users.noreply.github.com>
2023-03-07 15:06:04 +0800

    Update SA.py

--- SA.py
+++ SA.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-# Read the CSV file into a pandas DataFrame
 url = 'https://raw.githubusercontent.com/livetomuch/liveto/main/Advertising.csv'
 df = pd.read_csv(url)
 
-# Display the data in Streamlit using st.write()
 st.write(df)
